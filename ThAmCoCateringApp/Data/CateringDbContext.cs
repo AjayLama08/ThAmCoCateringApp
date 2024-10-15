2d126f537daca1d37f1da475691f89cc6dff5066
AJAY LAMA <Q2043246@live.tees.ac.uk>
2024-10-15 14:12:04 +0100

    Database updated

--- ThAmCoCateringApp/Data/CateringDbContext.cs
+++ ThAmCoCateringApp/Data/CateringDbContext.cs
@@ -2,7 +2,69 @@
 
 namespace ThAmCoCateringApp.Data
 {
-    internal class CateringDbContext
+    public class CateringDbContext : DbContext
     {
+        public DbSet<FoodBooking> FoodBookings { get; set; }
+        public DbSet<FoodItem> FoodItems { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuFoodItem> MenuFoodItems { get; set; }
+        public string DbPath { get; set; } = string.Empty;
+
+        public CateringDbContext()
+        {
+            var folder = Environment.SpecialFolder.MyDocuments;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "catering.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite($"Data source = {DbPath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //Compound key
+            builder.Entity<MenuFoodItem>().HasKey(mfi => new { mfi.MenuId, mfi.FoodItemId });
+
+            builder.Entity<FoodItem>()
+                .HasMany(mfi => mfi.MenuFoodItems)
+                .WithOne(fi => fi.FoodItem)
+                .HasForeignKey(fi => fi.FoodItemId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Menu>()
+                .HasMany(mfi => mfi.MenuFoodItems)
+                .WithOne(m => m.Menu)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Menu>().HasData(
+                new Menu { MenuId = 1, MenuName = "Vegetarian" },
+                new Menu { MenuId = 2, MenuName = "Non-Vegetarian" },
+                new Menu { MenuId = 3, MenuName = "Vegan" }
+            );
+
+            builder.Entity<FoodBooking>().HasData(
+                new FoodBooking { FoodBookingId = 1, ClientReferenceId = 101, NumberOfGuests = 10, MenuId = 1 },
+                new FoodBooking { FoodBookingId = 2, ClientReferenceId = 201, NumberOfGuests = 20, MenuId = 2 },
+                new FoodBooking { FoodBookingId = 3, ClientReferenceId = 301, NumberOfGuests = 30, MenuId = 3 }
+            );
+
+            builder.Entity<FoodItem>().HasData(
+                new FoodItem { FoodItemId = 1, Description = "Chicken curry", UnitPrice = 10 },
+                new FoodItem { FoodItemId = 2, Description = "Steamed rice", UnitPrice = 20 },
+                new FoodItem { FoodItemId = 3, Description = "Boiled eggs", UnitPrice = 30 }
+            );
+            builder.Entity<MenuFoodItem>().HasData(
+                new MenuFoodItem { MenuId = 1, FoodItemId = 1 },
+                new MenuFoodItem { MenuId = 2, FoodItemId = 2 },
+                new MenuFoodItem { MenuId = 3, FoodItemId = 3 }
+            );
+
+
+
+        }
     }
 }
---
+++ ThAmCoCateringApp/Data/Migrations/20241015130940_InitialCreate.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ThAmCoCateringApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FoodItems",
+                columns: table => new
+                {
+                    FoodItemId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    UnitPrice = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodItems", x => x.FoodItemId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    MenuId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MenuName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.MenuId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FoodBookings",
+                columns: table => new
+                {
+                    FoodBookingId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClientReferenceId = table.Column<int>(type: "INTEGER", nullable: false),
+                    NumberOfGuests = table.Column<int>(type: "INTEGER", nullable: false),
+                    MenuId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodBookings", x => x.FoodBookingId);
+                    table.ForeignKey(
+                        name: "FK_FoodBookings_Menus_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "Menus",
+                        principalColumn: "MenuId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MenuFoodItems",
+                columns: table => new
+                {
+                    MenuId = table.Column<int>(type: "INTEGER", nullable: false),
+                    FoodItemId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuFoodItems", x => new { x.MenuId, x.FoodItemId });
+                    table.ForeignKey(
+                        name: "FK_MenuFoodItems_FoodItems_FoodItemId",
+                        column: x => x.FoodItemId,
+                        principalTable: "FoodItems",
+                        principalColumn: "FoodItemId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MenuFoodItems_Menus_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "Menus",
+                        principalColumn: "MenuId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "FoodItems",
+                columns: new[] { "FoodItemId", "Description", "UnitPrice" },
+                values: new object[,]
+                {
+                    { 1, "Chicken curry", 10f },
+                    { 2, "Steamed rice", 20f },
+                    { 3, "Boiled eggs", 30f }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Menus",
+                columns: new[] { "MenuId", "MenuName" },
+                values: new object[,]
+                {
+                    { 1, "Vegetarian" },
+                    { 2, "Non-Vegetarian" },
+                    { 3, "Vegan" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "FoodBookings",
+                columns: new[] { "FoodBookingId", "ClientReferenceId", "MenuId", "NumberOfGuests" },
+                values: new object[,]
+                {
+                    { 1, 101, 1, 10 },
+                    { 2, 201, 2, 20 },
+                    { 3, 301, 3, 30 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "MenuFoodItems",
+                columns: new[] { "FoodItemId", "MenuId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 2 },
+                    { 3, 3 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FoodBookings_MenuId",
+                table: "FoodBookings",
+                column: "MenuId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuFoodItems_FoodItemId",
+                table: "MenuFoodItems",
+                column: "FoodItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FoodBookings");
+
+            migrationBuilder.DropTable(
+                name: "MenuFoodItems");
+
+            migrationBuilder.DropTable(
+                name: "FoodItems");
+
+            migrationBuilder.DropTable(
+                name: "Menus");
+        }
+    }
+}
